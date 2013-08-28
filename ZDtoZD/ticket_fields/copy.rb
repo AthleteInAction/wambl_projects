e6673307d366781dff1bf12460ecdcd7a86d8e93
onlyexcellence <will.robinson@myphotodynamic.com>
2013-08-28 05:11:42 -0700

    #2

--- ZDtoZD/ticket_fields/copy.rb
+++ ZDtoZD/ticket_fields/copy.rb
@@ -25,6 +25,7 @@ SystemFields = [
 
 
 # Get TARGET Fields
+target_key = {}
 rino = Rino::Tusk.new 10
 pages = (Target.GetFields(per_page: 1)[:body]['count'].to_f/100).ceil
 pages.times do |page|
@@ -36,6 +37,7 @@ pages.times do |page|
     call = Target.GetFields(page: page)
     call[:body]['ticket_fields'].each do |field|
       
+      target_key = target_key.merge(NW(field['title']) => field)
       JP %{[#{field['type']}] :: #{field['title']}\n}
       
     end
@@ -44,3 +46,28 @@ pages.times do |page|
   
 end
 rino.execute
+Write 'target.json',target_key.to_json
+
+# Get SOURCE Fields
+source_key = {}
+rino = Rino::Tusk.new 10
+pages = (Source.GetFields(per_page: 1)[:body]['count'].to_f/100).ceil
+pages.times do |page|
+  
+  page += 1
+  
+  rino.queue do
+    
+    call = Source.GetFields(page: page)
+    call[:body]['ticket_fields'].each do |field|
+      
+      source_key = source_key.merge(NW(field['title']) => field)
+      JP %{[#{field['type']}] :: #{field['title']}\n}
+      
+    end
+    
+  end
+  
+end
+rino.execute
+Write 'source.json',source_key.to_json
